9bb5271966905d8204d7399ca9d5a57b9bab3ea3
SarunasT <sarunas.tarvydas@hotmail.com>
2024-02-29 16:44:35 +0100

    simple Heading and type for props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+type HeadingProps = {
+  title: string;
+};
 
- function App() {
-  return (
-    <div>App</div>
-  )
+function Heading(props: HeadingProps) {
+  console.log('props ===', props);
+  return <h3>{props.title} visi</h3>;
 }
 
-export default App
+function App() {
+  return (
+    <div className='container'>
+      <h1 className='display-2'>React TS App</h1>
+      <Heading title='Sveiki' />
+    </div>
+  );
+}
+
+export default App;
